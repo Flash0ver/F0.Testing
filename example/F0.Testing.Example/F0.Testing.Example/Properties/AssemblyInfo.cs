4769a68517223f8795537829d0f56905025c79ec
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2019-06-23 11:10:03 +0200

    Add example projects
    * xUnit
    * NUnit
    * MSTest

---
+++ example/F0.Testing.Example/F0.Testing.Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("F0.Testing.Example.MSTest")]
+[assembly: InternalsVisibleTo("F0.Testing.Example.NUnit")]
+[assembly: InternalsVisibleTo("F0.Testing.Example.xUnit")]
